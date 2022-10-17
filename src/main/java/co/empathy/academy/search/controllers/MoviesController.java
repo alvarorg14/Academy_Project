deaad8c273d49b684f1bfdc47179bc249d3b6c52
Alvaro Rodriguez <alvamige@gmail.com>
2022-10-17 09:22:43 +0200

    Understanding SLF4j logger

--- src/main/java/co/empathy/academy/search/controllers/MoviesController.java
+++ src/main/java/co/empathy/academy/search/controllers/MoviesController.java
@@ -2,6 +2,7 @@ package co.empathy.academy.search.controllers;
 
 import co.empathy.academy.search.entities.Movie;
 import co.empathy.academy.search.services.MoviesService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@Slf4j
 public class MoviesController {
 
     @Autowired
@@ -17,13 +19,14 @@ public class MoviesController {
     @PostMapping(value = "/create")
     public String createMovie(@RequestBody Movie movie){
         moviesService.createMovie(movie);
-        return "correcto";
+        log.info("Pelicula creada correctamente: " + movie.toString());
+        return "Pelicula creada correctamente";
     }
 
     @GetMapping(value = "/findByTitle", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Movie> findByTitle(@RequestParam String title){
         List<Movie> movies = moviesService.findByTitle(title);
-        System.out.println(movies.get(0));
+        log.info("Returned movie with ID: " + movies.get(0).getId());
         return movies;
     }
 }
--- src/main/java/co/empathy/academy/search/services/MoviesService.java
+++ src/main/java/co/empathy/academy/search/services/MoviesService.java
@@ -2,7 +2,6 @@ package co.empathy.academy.search.services;
 
 import co.empathy.academy.search.entities.Movie;
 import co.empathy.academy.search.repositories.MoviesConector;
-import co.empathy.academy.search.repositories.MoviesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,17 +11,10 @@ import java.util.List;
 public class MoviesService{
 
     @Autowired
-    private MoviesRepository moviesRepository;
-
-    @Autowired
     private MoviesConector moviesConector;
 
     public void createMovie(Movie movie){
         moviesConector.indexMovie(movie);
-    }
-
-    public void createMovieBulk(List<Movie> movies){
-        moviesRepository.saveAll(movies);
     }
 
     public List<Movie> findByTitle(String title){
