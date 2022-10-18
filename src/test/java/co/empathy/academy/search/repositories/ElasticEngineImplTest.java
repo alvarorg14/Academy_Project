9a7d04a2fe31301816d4db7c9eb166e8a85c9d91
Alvaro Rodriguez <alvamige@gmail.com>
2022-10-18 13:08:28 +0200

    ElasticEngine tests

--- src/test/java/co/empathy/academy/search/repositories/ElasticEngineImplTest.java
+++ src/test/java/co/empathy/academy/search/repositories/ElasticEngineImplTest.java
@@ -1,11 +1,18 @@
 package co.empathy.academy.search.repositories;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.entity.BasicHttpEntity;
+import org.apache.http.entity.HttpEntityWrapper;
+import org.apache.http.util.EntityUtils;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpHeaders;
 
+import javax.websocket.RemoteEndpoint;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,10 +25,22 @@ class ElasticEngineImplTest {
 
     @Test
     void givenElasticUp_whenGetElasticInfo_thenElasticInfoReturned() throws IOException {
-        RestClient client = mock(RestClient.class);
-        given(client.performRequest(any())).willReturn(null); //TODO
+        String expectedResponse = "{\"cluster_name\":\"docker-cluster\"}";
 
-        //TODO
+        BasicHttpEntity entity = new BasicHttpEntity();
+        entity.setContent(new ByteArrayInputStream(expectedResponse.getBytes()));
+
+        Response mockResponse = mock(Response.class);
+        given(mockResponse.getEntity()).willReturn(entity);
+
+        RestClient client = mock(RestClient.class);
+        given(client.performRequest(any())).willReturn(mockResponse);
+
+        ElasticEngineImpl elasticEngine = new ElasticEngineImpl(client);
+
+        String response = elasticEngine.getElasticInfo();
+
+        assertEquals(expectedResponse, response);
     }
 
     @Test
