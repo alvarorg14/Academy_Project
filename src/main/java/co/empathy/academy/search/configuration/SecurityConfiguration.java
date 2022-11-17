befde634cbccb7372521ca988c8f8d6e6987899a
Alvaro Rodriguez <alvamige@gmail.com>
2022-11-17 08:09:49 +0100

    CORS Updated

--- src/main/java/co/empathy/academy/search/configuration/SecurityConfiguration.java
+++ src/main/java/co/empathy/academy/search/configuration/SecurityConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -14,13 +14,14 @@ import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .cors().configurationSource(corsConfigurationSource());
+        return http.build();
     }
 
     @Bean
