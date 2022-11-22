1d1772e4e3f7a5a7aedcdfa5dd91da69b2f95e7c
Alvaro Rodriguez <alvamige@gmail.com>
2022-11-22 10:28:41 +0100

    Script second version

--- script.sh
+++ script.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
+
+echo "Starting elastic and API containers..."
 docker-compose -f docker-compose.back.yml up --build -d
 sleep 10
 
+echo "Creating the index..."
 curl -X PUT http://localhost:8080/index/create
+
+echo "Indexing the data..."
 curl -F basicsFile=@IMDb/title.basics.tsv -F ratingsFile=@IMDb/title.ratings.tsv -F akasFile=@IMDb/title.akas.tsv -F crewFile=@IMDb/title.crew.tsv http://localhost:8080/index/imdb
+
+echo "Stopping containers..."
+docker-compose down
+
+echo "Creating image..."
+docker commit elasticsearch alvarorg14/imdb:es_index
+
+echo "Pushing image to Docker Hub..."
+docker push alvarorg14/imdb:es_index
