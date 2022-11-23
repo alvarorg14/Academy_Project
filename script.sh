0683961fea7435d8a0fd6dc19c3cc9bf88f83e82
Alvaro Rodriguez <alvamige@gmail.com>
2022-11-23 10:51:49 +0100

    Script third version

--- script.sh
+++ script.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+echo "Extracting data..."
+unzip -o IMDbData.zip
+
 echo "Starting elastic and API containers..."
 docker-compose -f docker-compose.back.yml up --build -d
-sleep 10
+sleep 30
 
 echo "Creating the index..."
 curl -X PUT http://localhost:8080/index/create
 
 echo "Indexing the data..."
-curl -F basicsFile=@IMDb/title.basics.tsv -F ratingsFile=@IMDb/title.ratings.tsv -F akasFile=@IMDb/title.akas.tsv -F crewFile=@IMDb/title.crew.tsv http://localhost:8080/index/imdb
+curl -F basics=@IMDbData/title.basics.tsv -F ratings=@IMDbData/title.ratings.tsv -F akas=@IMDbData/title.akas.tsv -F crew=@IMDbData/title.crew.tsv -F principals=@IMDbData/title.principals.tsv http://localhost:8080/index/imdb
 
 echo "Stopping containers..."
 docker-compose down
